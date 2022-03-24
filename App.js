dcc47979a2ce6fd6e7fb11c709c82d3f67b7c798
Sondinh <dnson20022001@gmail.com>
2022-03-24 15:44:26 +0700

    commit add basic editor && preview

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
     StyleSheet,
@@ -6,13 +6,26 @@ import {
     View,
     useWindowDimensions,
     ScrollView,
+    TouchableOpacity,
+    TextInput,
 } from "react-native";
 import RenderHtml from "react-native-render-html";
 import markdownProcessor from "./utils/markdownProcessor";
 import Markdown from "react-native-markdown-display";
 
 export default function App() {
-    const { width } = useWindowDimensions();
+    const [togglePreview, setTogglePreview] = useState(false);
+    const [note, setNote] = useState("");
+    const handleTogglePreview = () => {
+        setTogglePreview(!togglePreview);
+    };
+
+    const handleChange = (value) => {
+        console.log(value);
+        setNote(value);
+    };
+
+    const { width, height } = useWindowDimensions();
     const copy =
         "# Hi\n\n## Table of contents\n\n### Hello\n\n**Some** ~more~ _things_.\n ```js\nconsole.log('hello world')\n```";
     return (
@@ -22,13 +35,32 @@ export default function App() {
                 contentWidth={width}
                 source={{ html: markdownProcessor() }}
             /> */}
+            <TouchableOpacity onPress={() => handleTogglePreview()}>
+                <Text>Preview</Text>
+            </TouchableOpacity>
             <ScrollView
                 contentInsetAdjustmentBehavior="automatic"
+                keyboardDismissMode="interactive"
                 style={{ height: "100%", width: width }}
             >
-                <Markdown style={{ body: { fontFamily: "sans-serif" } }}>
-                    {copy}
-                </Markdown>
+                {togglePreview ? (
+                    <Markdown style={{ body: { fontFamily: "sans-serif" } }}>
+                        {note}
+                    </Markdown>
+                ) : (
+                    <TextInput
+                        multiline={true}
+                        autoFocus={true}
+                        placeholder="write your code here"
+                        style={{
+                            height: height - 80,
+                            backgroundColor: "black",
+                            color: "white",
+                            fontSize: 16,
+                        }}
+                        onChange={(e) => handleChange(e.nativeEvent.text)}
+                    ></TextInput>
+                )}
             </ScrollView>
         </View>
     );
