d90c04a8b8ae14d3bcb0acf13c1712532f65738c
Sondinh <dnson20022001@gmail.com>
2022-03-24 10:40:05 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+    StyleSheet,
+    Text,
+    View,
+    useWindowDimensions,
+    ScrollView,
+} from "react-native";
+import RenderHtml from "react-native-render-html";
+import markdownProcessor from "./utils/markdownProcessor";
+import Markdown from "react-native-markdown-display";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const { width } = useWindowDimensions();
+    const copy =
+        "# Hi\n\n## Table of contents\n\n### Hello\n\n**Some** ~more~ _things_.\n ```js\nconsole.log('hello world')\n```";
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            {/* <RenderHtml
+                contentWidth={width}
+                source={{ html: markdownProcessor() }}
+            /> */}
+            <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                style={{ height: "100%", width: width }}
+            >
+                <Markdown style={{ body: { fontFamily: "sans-serif" } }}>
+                    {copy}
+                </Markdown>
+            </ScrollView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        fontFamily: "sans-serif",
+    },
 });
