2390f6ba7b8bb04d5ecccdb1240a09f61ed6bb54
Sondinh <dnson20022001@gmail.com>
2022-05-14 11:06:17 +0700

    commit code

---
+++ src/screens/Notes/Notes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, ScrollView, View, TextInput } from "react-native";
+
+function Notes() {
+    return (
+        <View>
+            <Text>Hello, Nam! 👋🏻</Text>
+            <TextInput placeholder="Search your note..."></TextInput>
+        </View>
+    );
+}
+
+export default Notes;
