83504056d9d0aceaff92c532cd789c7d11df1799
Sondinh <dnson20022001@gmail.com>
2022-05-17 00:05:23 +0700

    commit code

--- src/components/NotesList/NotesList.js
+++ src/components/NotesList/NotesList.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { View, ScrollView, Text, TouchableOpacity } from "react-native";
-import tw from "twrnc";
-import Icon from "react-native-vector-icons/MaterialIcons";
+import React from 'react';
+import { View, ScrollView, Text, TouchableOpacity } from 'react-native';
+import tw from 'twrnc';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 function NotesList() {
     return (
-        <View>
-            <View style={tw` flex flex-row justify-around`}>
+        <View style={tw`w-full`}>
+            <View style={tw`mb-2 flex-row justify-around`}>
                 <TouchableOpacity>
                     <Text>All Notes</Text>
                 </TouchableOpacity>
@@ -17,9 +17,12 @@ function NotesList() {
                     <Text>Highlighted</Text>
                 </TouchableOpacity>
             </View>
-            <ScrollView style={tw`mt-4`}>
+            <ScrollView
+                style={tw`mt-2 border-red-500 border-2`}
+                // contentContainerStyle={{ paddingBottom: '55%' }}
+            >
                 <View style={tw`p-3 mb-3 bg-[#A8D7E0] rounded-lg`}>
-                    <View style={tw`flex flex-row items-center`}>
+                    <View style={tw`flex-row items-center`}>
                         <Icon name="description" size={22} color="black"></Icon>
                         <Text style={tw`ml-1 text-base`}>Cooking task</Text>
                     </View>
@@ -29,9 +32,7 @@ function NotesList() {
                         quia dolore eum provident quibusdam architecto nam
                         veniam dolorem modi quis earum rem neque explicabo?
                     </Text>
-                    <View
-                        style={tw`flex flex-row justify-between text-slate-400`}
-                    >
+                    <View style={tw`flex-row justify-between text-slate-400`}>
                         <Text>Cook | Ongoing</Text>
                         <Text>05/04/2022</Text>
                     </View>
@@ -47,15 +48,13 @@ function NotesList() {
                         quia dolore eum provident quibusdam architecto nam
                         veniam dolorem modi quis earum rem neque explicabo?
                     </Text>
-                    <View
-                        style={tw`flex flex-row justify-between text-slate-400`}
-                    >
+                    <View style={tw`flex-row justify-between text-slate-400`}>
                         <Text>Cook | Ongoing</Text>
                         <Text>05/04/2022</Text>
                     </View>
                 </View>
                 <View style={tw`p-3 mb-3 bg-[#A8D7E0] rounded-lg`}>
-                    <View style={tw`flex flex-row items-center`}>
+                    <View style={tw`flex-row items-center`}>
                         <Icon name="description" size={22} color="black"></Icon>
                         <Text style={tw`ml-1 text-base`}>Cooking task</Text>
                     </View>
@@ -65,15 +64,13 @@ function NotesList() {
                         quia dolore eum provident quibusdam architecto nam
                         veniam dolorem modi quis earum rem neque explicabo?
                     </Text>
-                    <View
-                        style={tw`flex flex-row justify-between text-slate-400`}
-                    >
+                    <View style={tw`flex-row justify-between text-slate-400`}>
                         <Text>Cook | Ongoing</Text>
                         <Text>05/04/2022</Text>
                     </View>
                 </View>
                 <View style={tw`p-3 mb-3 bg-[#A8D7E0] rounded-lg`}>
-                    <View style={tw`flex flex-row items-center`}>
+                    <View style={tw`flex-row items-center`}>
                         <Icon name="description" size={22} color="black"></Icon>
                         <Text style={tw`ml-1 text-base`}>Cooking task</Text>
                     </View>
@@ -83,15 +80,13 @@ function NotesList() {
                         quia dolore eum provident quibusdam architecto nam
                         veniam dolorem modi quis earum rem neque explicabo?
                     </Text>
-                    <View
-                        style={tw`flex flex-row justify-between text-slate-400`}
-                    >
+                    <View style={tw`flex-row justify-between text-slate-400`}>
                         <Text>Cook | Ongoing</Text>
                         <Text>05/04/2022</Text>
                     </View>
                 </View>
                 <View style={tw`p-3 mb-3 bg-[#A8D7E0] rounded-lg`}>
-                    <View style={tw`flex flex-row items-center`}>
+                    <View style={tw`flex-row items-center`}>
                         <Icon name="description" size={22} color="black"></Icon>
                         <Text style={tw`ml-1 text-base`}>Cooking task</Text>
                     </View>
@@ -101,15 +96,13 @@ function NotesList() {
                         quia dolore eum provident quibusdam architecto nam
                         veniam dolorem modi quis earum rem neque explicabo?
                     </Text>
-                    <View
-                        style={tw`flex flex-row justify-between text-slate-400`}
-                    >
+                    <View style={tw`flex-row justify-between text-slate-400`}>
                         <Text>Cook | Ongoing</Text>
                         <Text>05/04/2022</Text>
                     </View>
                 </View>
                 <View style={tw`p-3 mb-3 bg-[#A8D7E0] rounded-lg`}>
-                    <View style={tw`flex flex-row items-center`}>
+                    <View style={tw`flex-row items-center`}>
                         <Icon name="description" size={22} color="black"></Icon>
                         <Text style={tw`ml-1 text-base`}>Cooking task</Text>
                     </View>
@@ -119,15 +112,13 @@ function NotesList() {
                         quia dolore eum provident quibusdam architecto nam
                         veniam dolorem modi quis earum rem neque explicabo?
                     </Text>
-                    <View
-                        style={tw`flex flex-row justify-between text-slate-400`}
-                    >
+                    <View style={tw`flex-row justify-between text-slate-400`}>
                         <Text>Cook | Ongoing</Text>
                         <Text>05/04/2022</Text>
                     </View>
                 </View>
                 <View style={tw`p-3 mb-3 bg-[#A8D7E0] rounded-lg`}>
-                    <View style={tw`flex flex-row items-center`}>
+                    <View style={tw`flex-row items-center`}>
                         <Icon name="description" size={22} color="black"></Icon>
                         <Text style={tw`ml-1 text-base`}>Cooking task</Text>
                     </View>
@@ -137,9 +128,36 @@ function NotesList() {
                         quia dolore eum provident quibusdam architecto nam
                         veniam dolorem modi quis earum rem neque explicabo?
                     </Text>
-                    <View
-                        style={tw`flex flex-row justify-between text-slate-400`}
-                    >
+                    <View style={tw`flex-row justify-between text-slate-400`}>
+                        <Text>Cook | Ongoing</Text>
+                        <Text>05/04/2022</Text>
+                    </View>
+                </View>
+                <View style={tw`p-3 mb-3 bg-[#A8D7E0] rounded-lg`}>
+                    <View style={tw`flex-row items-center`}>
+                        <Icon name="description" size={22} color="black"></Icon>
+                        <Text style={tw`ml-1 text-base`}>Cooking task</Text>
+                    </View>
+                    <Text style={tw`text-[15px] py-1`} numberOfLines={3}>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Est aut similique, hic voluptatem rerum obcaecati
+                        quia dolore eum provident quibusdam architecto nam
+                        veniam dolorem modi quis earum rem neque explicabo?
+                    </Text>
+                    <View style={tw`flex-row justify-between text-slate-400`}>
+                        <Text>Cook | Ongoing</Text>
+                        <Text>05/04/2022</Text>
+                    </View>
+                </View>
+                <View style={tw`p-3 mb-3 bg-[#A8D7E0] rounded-lg`}>
+                    <View style={tw`flex-row items-center`}>
+                        <Icon name="description" size={22} color="black"></Icon>
+                        <Text style={tw`ml-1 text-base`}>Cooking task</Text>
+                    </View>
+                    <Text style={tw`text-[15px] py-1`} numberOfLines={3}>
+                        something something
+                    </Text>
+                    <View style={tw`flex-row justify-between text-slate-400`}>
                         <Text>Cook | Ongoing</Text>
                         <Text>05/04/2022</Text>
                     </View>
--- src/screens/Notes/Notes.js
+++ src/screens/Notes/Notes.js
@@ -1,28 +1,34 @@
-import React from "react";
+import React from 'react';
 import {
     Text,
     ScrollView,
     View,
     TextInput,
     TouchableOpacity,
-} from "react-native";
-import NotesList from "../../components/NotesList/NotesList";
-import tw from "twrnc";
+    Dimensions,
+} from 'react-native';
+import NotesList from '../../components/NotesList/NotesList';
+import tw from 'twrnc';
 
 function Notes() {
+    const height = Dimensions.get('window').height;
     return (
-        <View style={tw`flex flex-column px-5 py-2.5 bg-white`}>
+        <View
+            style={tw.style('flex', 'flex-col', 'px-5', 'py-2.5', 'bg-white', {
+                height: height,
+            })}
+        >
             <Text style={tw`text-[28px]`}>Hello, Nam! 👋🏻</Text>
             <TextInput
                 placeholder="Search your note..."
                 style={tw`p-3.5 my-2.5 bg-neutral-100 text-[16px] rounded-lg`}
             ></TextInput>
             <NotesList></NotesList>
-            <TouchableOpacity
+            {/* <TouchableOpacity
                 style={tw`absolute bottom-2.5 right-2.5 bg-[#384D95] rounded-full`}
             >
                 <Text>+</Text>
-            </TouchableOpacity>
+            </TouchableOpacity> */}
         </View>
     );
 }
